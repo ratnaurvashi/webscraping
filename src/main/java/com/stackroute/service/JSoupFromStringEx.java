3693f916a907994d50a08dba444302d5bf61c44a
urvashi.ratna <urvashi.ratna@boeing.com>
2019-03-06 15:23:04 +0530

    added more functionality

--- src/main/java/com/stackroute/service/JSoupFromStringEx.java
+++ src/main/java/com/stackroute/service/JSoupFromStringEx.java
@@ -8,28 +8,26 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class JSoupFromStringEx {
 
     public static void main(String[] args) {
 
         final String url =
-                "http://ogp.me/";
+                "https://www.tutorialspoint.com/spring/spring_hello_world_example.htm";
 
         try {
             final Document document = Jsoup.connect(url).get();
-            System.out.println("CONTENT: " + document.outerHtml());
+            //getting the whole content of the webpage
+            String webContent=document.outerHtml();
+            System.out.println("CONTENT: " + webContent);
+            //getting the title of the webpage
             String title = document.title();
-            String body = document.body().text();
             System.out.println();
             System.out.println("TITLE: " + title);
             System.out.println();
-            System.out.println("BODY: " + body);
-            System.out.println();
 
-
+            //getting the meta tags
             Elements metaTags = document.getElementsByTag("meta");
             List<JSONObject> metadata = new ArrayList<>();
 
@@ -37,18 +35,22 @@ public class JSoupFromStringEx {
             String keywords = "";
             for (Element mTag : metaTags) {
                 JSONObject metatag = new JSONObject();
+                //storing the meta tag properties
                 String content = mTag.attr("content");
                 String name = mTag.attr("name");
                 String property = mTag.attr("property");
                 if (!name.equals("")) {
                     if (name.equals("description")) {
+                        //getting the description meta tag
                         description = document.select("meta[name=description]").first().attr("content");
                     }
                     if (name.equals("keywords")) {
+                        //getting the keywords meta tag
                         keywords = document.select("meta[name=keywords]").first().attr("content");
                     }
                     metatag.put("name", name);
                     metatag.put("content", content);
+                    //adding the meta tags in json list metadata
                     metadata.add(metatag);
 
                 }
@@ -61,6 +63,7 @@ public class JSoupFromStringEx {
                     }
                     metatag.put("property", property);
                     metatag.put("content", content);
+                    //adding the meta tags in JSONObject list metadata
                     metadata.add(metatag);
                 }
 
@@ -73,6 +76,7 @@ public class JSoupFromStringEx {
             System.out.println("KEYWORDS : " + keywords);
             System.out.println();
 
+            //counting the number of images on the html page
             int imgcnt = 0;
             Elements images = document.select("img[src~=(?i)\\.(png|jpe?g|gif)]");
             for (Element image : images) {
@@ -80,21 +84,16 @@ public class JSoupFromStringEx {
             }
             System.out.println("IMAGE COUNT: " + imgcnt);
 
-
-//            Pattern p = Pattern.compile("<pre>[^>]*>(.+?)</pre>");
-//            Matcher m = p.matcher(document.toString());
-            Elements code = document.getElementsByTag("pre");
+            //calculating all the pre code tags and counting the number of times they occur
+            Elements code = document.getElementsByTag("pre").tagName("code");
             float codecnt=0;
             for (Element codeElem : code) {
                 codecnt++;
             }
-            // if we find a match, get the group
-//            if (m.find()) {
-//                codecnt++;
-//            }
             System.out.println();
-            System.out.println("NUMBER OF PRE TAGS: "+codecnt);
+            System.out.println("NUMBER OF PRE CODE TAGS: "+codecnt);
 
+            //getting all tags and counting the number of times they occur
             Elements alltags = document.getAllElements();
             float tagcount=0;
             for (Element allTag : alltags) {
@@ -104,8 +103,27 @@ public class JSoupFromStringEx {
             System.out.println();
             System.out.println("NUMBER OF TOTAL TAGS: "+tagcount);
 
+            //calculating code percentage
             float codePercentage = (codecnt/tagcount) * 100;
+            System.out.println();
             System.out.println("CODE PERCENTAGE: "+ codePercentage);
+
+            //Extracting paragraphs and giving them each a unique id
+            Elements paragraphTag = document.getElementsByTag("p");
+            int paracnt=0;
+            List<JSONObject> paragraphs = new ArrayList<>();
+            for (Element paraString : paragraphTag) {
+                JSONObject paragraph = new JSONObject();
+                paracnt++;
+                paragraph.put("paraId",paracnt);
+                //extracting content between <p> and </p> tag
+                String para = paraString.toString().split("</p>")[0];
+                para = para.split("<p>")[1];
+                paragraph.put("paragraph",para);
+                paragraphs.add(paragraph);
+            }
+            System.out.println();
+            System.out.println("PARAGRAPH JSON: "+paragraphs);
         }
         catch (Exception ex){
             ex.printStackTrace();
